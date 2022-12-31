fd1837763e2a2f966c238fd0bbb0423060030a6f
chungliqing <chungliqing21@gmail.com>
2022-12-31 18:59:24 +0800

    chore: Part 1 Fundamentals; 2. Types; 2. Variables;

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int age = 30, temperature = 20; //kinda messy, better to separate
+        age = 35; // change variable's value
+        int herAge = age;
+        System.out.println("Hello World!");
+        System.out.println(age);
+        System.out.println(herAge);
     }
 }
