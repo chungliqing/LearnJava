13fc918d23c03fbc5291d33009455df2f6d9d62a
chungliqing <chungliqing21@gmail.com>
2022-12-31 20:16:22 +0800

    Part 1 Fundamentals; 2. Types; 4. Reference Types;

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
+import java.util.Date;
+
 public class App {
     public static void main(String[] args) {
-        primitiveTypes();
+        referenceTypes();
         
     }
 
@@ -14,8 +16,8 @@ public class App {
     }
 
     public static void primitiveTypes() {
-        /*  Primitive Types- for storing simple values
-            Reference Types - for storing complex objects
+        /*  Primitive Types- for storing simple values (int, bool, char)
+            Reference Types - for storing complex objects (date, mail messages)
 
             Primitive Types- for storing simple values
                 Type        Bytes       Range           Notes
@@ -37,5 +39,38 @@ public class App {
         float price = 10.99F; //by default the compiler sees the dec # as a double
         char letter = 'A'; //char uses single quotes
         boolean isEligible = true; //blue colored words are reserved keywords
+    }
+
+    public static void referenceTypes(){
+        /*  
+            *Primitive Types    -   for storing simple values (int, bool, char)
+                                -   memory/variables are auto allocated & released by Java Runtime Environment (JRE)
+                                -   no members/members; but have code snippets (templates/code block) through dot op
+                                        -   age.fori
+
+            *Reference Types    -   for storing complex objects (date - ref classes/methods; mail messages)
+                                -   memory has to be manually allocated, but JRE will auto release
+                                -   'new' to manually allocate memory for reference type variables  
+                                -   Objects/Classes have members/methods accessible through the dot operator
+                                        -   now.getTime()
+        */
+
+        byte age = 30; //Primitive Type
+        Date now = new Date(); //Reference Type
+        System.out.println(now); //prints 'Sat Dec 31 20:06:18 MYT 2022'
+
+        System.out.println(now.getTime());
+
+        /*
+            *java.util/java.sql = packages;
+                typing 'Date' and selecting 'java.util' auto imports the java.util.Date package
+  
+            *now becomes an instance/object of the Date() class; 
+                class == template/blueprint for creating new objects/instances;
+                an object is an instance of a class; object = class's instance
+                
+                Example:
+                    *class == humans; --> instances of classes == objects like John, Mary, etc.
+        */
     }
 }
