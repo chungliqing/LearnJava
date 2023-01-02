9f8ba4403ae618a35375bf23af8364414d137ce0
chungliqing <chungliqing21@gmail.com>
2023-01-02 23:58:52 +0800

    Part 1 Fundamentals; 3. Control Flow; 3. Logical Operators;

--- src/Fundamentals_ControlFlow.java
+++ src/Fundamentals_ControlFlow.java
@@ -1,7 +1,7 @@
 
 public class Fundamentals_ControlFlow{
 	public static void main(String[] args) {
-		comparisonOperators();
+		logicalOperators();
     }
 
 	public static void comparisonOperators() {
@@ -9,8 +9,19 @@ public class Fundamentals_ControlFlow{
 		int y = 1;
 		System.out.println(x == y); //prints true = equal
 		System.out.println(x != y); //prints false = not equal
-		System.out.println(x <= y); //prints false = not equal
+		System.out.println(x <= y); //prints true 
 		// >, >=, <, <=, 
-		
+	}
+
+	public static void logicalOperators() {
+		int temperature = 22;
+		boolean isWarm = temperature > 20 && temperature < 30; //looks from left to right
+		System.out.println(isWarm); //prints true
+
+		boolean hasHighIncome = false;
+		boolean hasGoodCredit = true;
+		boolean hasCriminalRecord = false;
+		boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+		System.out.println(isEligible);		
 	}
 }
