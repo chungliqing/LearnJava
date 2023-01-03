dafb173e5545a4bb44915c266e88d97afab0e104
chungliqing <chungliqing21@gmail.com>
2023-01-03 23:54:12 +0800

    Part 2 OOP; 2. Classes; 13. Method Overloading;

--- src/Employee.java
+++ src/Employee.java
@@ -1,16 +1,39 @@
 public class Employee {
-	public int baseSalary;
-	public int hourlyRate;
+	private int baseSalary;
+	private int hourlyRate;
+
+	public Employee(int baseSalary, int hourlyRate){
+		setBaseSalary(baseSalary);
+		setHourlyRate(hourlyRate);
+	}
+
+	private int getHourlyRate() {
+		return hourlyRate;
+	}
+
+	public void setHourlyRate(int hourlyRate) {
+		if (hourlyRate <= 0)
+			throw new IllegalArgumentException("Hourly cannot be 0 or negative.");
+		this.hourlyRate = hourlyRate;
+	}
 
 	public int calculateWage(int extraHours){ //no parameters coz the para are on top
 		return baseSalary + (hourlyRate * extraHours);
 	}
 
-	public void setBaseSalary(int baseSalary){
+	public int calculateWage(){ //overloading method for calculate wage, if the value is 0/no extra hours
+		return baseSalary; 
+		//or return calculateWage(0);
+	}
+
+	private void setBaseSalary(int baseSalary){ //setters
 		if (baseSalary <= 0)
-			throw new IllegalArgumentException("Salary cannot be 0 or less."); //create new instance of this class
+			throw new IllegalArgumentException("Salary cannot be 0 or less."); //create new instance of this class; terminated and show error message.
 		this.baseSalary = baseSalary;
 	}
 
+	private int getBaseSalary(){ //getters //create fields as private and use setters/getters
+		return baseSalary;
+	}
 	
 }
