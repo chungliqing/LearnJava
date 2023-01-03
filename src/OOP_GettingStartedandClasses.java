840548a1689b7b76c0c0e838e3b1c7f34aede55d
chungliqing <chungliqing21@gmail.com>
2023-01-03 17:27:27 +0800

    Part 2 Object-Oriented Programming; 2. Classes; 2. Classes and Objects;

---
+++ src/OOP_GettingStartedandClasses.java
@@ -0,0 +1,46 @@
+public class OOP_GettingStartedandClasses{
+	public static void main(String[] args){
+
+	}
+
+	public static void programmingParadigms() {
+		/*
+			* Programming Paradigms
+				* Procedural
+				* Functional		-	Data, Behavior	-	separate
+				* Object-Oriented	-	Data (State), Methods (Behaviors) = Object
+				* Event-Driven
+				* Logic
+				* Aspect-Oriented 
+		 */
+	}
+
+	public static void objectOrientedProgramming() {
+		/*
+		 * Benefits of OOP
+		 	* Reduced Complexity
+			* Easier Maintenance
+			* Code Reuse
+			* Faster Development
+		 */
+	}
+
+	public static void classesAndObjects(){
+		/*
+		 	* Class = a blueprint for creating objects
+				Class Name = Car
+				Attributes = started, currentSpped, currentGear //implement these attributes using fields (variables def in class)
+				Methods/Behaviors = start(), stop(), brake()
+
+				Lamp					TV
+				isOn = boolean			currentVolume, currentChannel, isOn
+				turnOn(), turnOff()		turnOn(), turnOff(), increaseVolume(), decreaseVolume()
+
+				TextBox
+				text, limit, length, isFocused, isDisabled
+				setText(), clear(), enable(), focus()
+
+			* Object = an instance of a class //car1, car2, crv, hrv; independent; separate memory and states
+		 */
+	}
+}
