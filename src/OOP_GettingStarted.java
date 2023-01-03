0470f4160087596af7c750d52812d2f815f30c6d
chungliqing <chungliqing21@gmail.com>
2023-01-03 17:15:48 +0800

    Part 2 Object-Oriented Programming; 1. Getting Started; 3. Benefits of Object-Oriented Programming;

---
+++ src/OOP_GettingStarted.java
@@ -0,0 +1,27 @@
+public class OOP_GettingStarted{
+	public static void main(String[] args){
+
+	}
+
+	public static void programmingParadigms() {
+		/*
+			* Programming Paradigms
+				* Procedural
+				* Functional		-	Data, Behavior	-	separate
+				* Object-Oriented	-	Data (State), Methods (Behaviors) = Object
+				* Event-Driven
+				* Logic
+				* Aspect-Oriented 
+		 */
+	}
+
+	public static void objectOrientedProgramming() {
+		/*
+		 * Benefits of OOP
+		 	* Reduced Complexity
+			* Easier Maintenance
+			* Code Reuse
+			* Faster Development
+		 */
+	}
+}
