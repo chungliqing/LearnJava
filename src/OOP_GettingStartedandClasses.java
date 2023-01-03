9b793ed95b5cecb1528938b94be269fc07fa985e
chungliqing <chungliqing21@gmail.com>
2023-01-03 18:01:45 +0800

    Part 2 Object-Oriented Programming; 2. Classes; 4. Creating Objects;

--- src/OOP_GettingStartedandClasses.java
+++ src/OOP_GettingStartedandClasses.java
@@ -43,4 +43,33 @@ public class OOP_GettingStartedandClasses{
 			* Object = an instance of a class //car1, car2, crv, hrv; independent; separate memory and states
 		 */
 	}
+
+	public static void creatingClasses() {
+		/*	add each class to a separate file
+			Naming Convention = PascalCasing = TextBox
+			public = access modifier; whether other classes in the project can see/use this method
+			see TextBox.java
+		*/
+	}
+
+	public static void creatingObjects() {
+		//TextBox textBox1 = new TextBox();
+		var textBox1 = new TextBox();
+		textBox1.setText("Box 1"); //use the dot op to look at the members of this class
+		System.out.println(textBox1.text.toUpperCase()); //prints Box 1
+
+		var textBox2 = new TextBox();
+		textBox2.setText("Box 2"); // these instances operate independently with different states
+
+		/* 
+			* create a TextBox variable, by initializng with the new operator for the TextBox class
+			* new TextBox() = instantiating the TextBox class = creating a new instance of this class
+			* can replace Textbox on the left with var, and java will detect the variable type based on the assignment operator
+			* different from JavaScript where var is used to declare a variable
+
+			* if ref types are not initialized, they're set to null, which can lead to crashing
+
+			* null pointer exists when something it's pointing to doesn't exist
+		*/
+	}
 }
