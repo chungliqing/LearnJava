f58543ccaef4182ac024c9e73796e7d991cee8f4
chungliqing <chungliqing21@gmail.com>
2023-01-03 17:41:03 +0800

    Part 2 Object-Oriented Programming; 2. Classes; 4. Creating Classes;

---
+++ src/TextBox.java
@@ -0,0 +1,23 @@
+public class TextBox {
+	//add 3 members, TextField(use to store text in a textbox)
+	//1 field, 2 methods; 
+	/*
+	 * Steps to add a member/field/attribute
+	 		1. public - access modifier (usually fields arent declared public)
+			2. type of the field
+			3. give name using camelNotation
+	 */
+	public String text; //Field
+
+	//declare methods
+	public void setText(String text){
+		//if fieldName and parameters names are the same, use this keyword to ref the current object
+
+		//every class declared auto inherits methods like equals, toString...
+		this.text = text;
+	}
+
+	public void clear(){
+		text = "";
+	}
+}
